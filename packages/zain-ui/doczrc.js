2f7459913606928be0bb34974257ec1fc679a4c2
ZainChen <2384439266@qq.com>
2020-08-30 03:14:46 +0800

    update docs

--- packages/zain-ui/doczrc.js
+++ packages/zain-ui/doczrc.js
@@ -1,6 +1,8 @@
 export default {
-    title: "Zain UI",
+    title: 'Zain UI',
     menu: ['Getting Started', 'Components'],
+    base: '/zain-ui',
+    dest: '/build',
     typescript: true,
     themeConfig: {
         /** 默认代码不展开 */
