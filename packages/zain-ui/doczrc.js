b94be3390358b530927f0d95113ec64eb4e057c8
ZainChen <2384439266@qq.com>
2020-08-30 03:41:06 +0800

    update docs

--- packages/zain-ui/doczrc.js
+++ packages/zain-ui/doczrc.js
@@ -2,7 +2,7 @@ export default {
     title: 'Zain UI',
     menu: ['Getting Started', 'Components'],
     /** GitHub Pages 指定在哪个子目录中部署文件 */
-    base: '/docz',
+    base: '/zain-ui',
     /** build 构建输出目标文件夹 */
     dest: '../../docs',
     typescript: true,
