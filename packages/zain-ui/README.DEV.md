5c332c10e8b45d594f83149e9ab371110f16bd36
ZainChen <2384439266@qq.com>
2020-08-30 13:21:37 +0800

    update zain-ui

---
+++ packages/zain-ui/README.DEV.md
@@ -0,0 +1,7 @@
+# 开发说明文档
+
+## GitHub Pages 部署问题
+
+GitHub Pages 域名大小写敏感：
+
+`mdx` 文件中的路由，一定要全小写 `route: /components/menuList`
--- packages/zain-ui/doczrc.js
+++ packages/zain-ui/doczrc.js
@@ -1,6 +1,8 @@
 export default {
     title: 'Zain UI',
     menu: ['Getting Started', 'Components'],
+    /** 忽略文件 */
+    ignore: ['README.DEV.md'],
     /** GitHub Pages 指定在哪个子目录中部署文件 */
     base: '/zain-ui',
     /** build 构建输出目标文件夹 */
