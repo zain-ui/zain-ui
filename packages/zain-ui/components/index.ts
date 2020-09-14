b4e11f3531e102e2abe3198221a135345cb4fd49
ZainChen <2384439266@qq.com>
2020-09-15 01:34:23 +0800

    update components index.ts

--- packages/zain-ui/components/index.ts
+++ packages/zain-ui/components/index.ts
@@ -5,9 +5,9 @@ export * from './button';
  * Menu 和 MenuList 是同一个组件
  * Menu: 用 class 实现，样式用 js 生成，不依赖 css 文件
  * MenuList: 用 class 实现，样式用 less 生成，依赖 css 文件
+ * 启用 MenuList:
+ * export { default as MenuList } from './menuList';
+ * export * from './menuList';
  */
 export { default as Menu } from './menu';
 export * from './menu';
-
-// export { default as MenuList } from './menuList';
-// export * from './menuList';
